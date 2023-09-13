a2103a203ff45e17b33861afff7f9e5429615ef2
patrickpbarroso <patrick.plbarroso@gmail.com>
2023-09-13 10:10:41 -0300

    Tratamento de erros caso arquivo não abra

--- JogoDaForca/forca.cpp
+++ JogoDaForca/forca.cpp
@@ -92,17 +92,27 @@ vector<string> le_arquivo(){
     ifstream arquivo;
     arquivo.open("palavras.txt");
 
-    int quantidade_palavras;
-    arquivo >> quantidade_palavras;
+    // Verifica se o arquivo está aberto e realiza a leitura
+    if(arquivo.is_open()){
+        int quantidade_palavras;
+        arquivo >> quantidade_palavras;
 
-    vector<string> palavras_do_arquivo;
+        vector<string> palavras_do_arquivo;
 
-    for(int i=0;i<quantidade_palavras;i++){
-        string palavra_lida;
-        arquivo >> palavra_lida;
-        palavras_do_arquivo.push_back(palavra_lida);
+        for(int i=0;i<quantidade_palavras;i++){
+            string palavra_lida;
+            arquivo >> palavra_lida;
+            palavras_do_arquivo.push_back(palavra_lida);
+        }
+
+        arquivo.close();
+        return palavras_do_arquivo;
     }
-    return palavras_do_arquivo;
+    else
+    {
+        cout << "Não foi possível acessar o banco de palavras. " << endl;
+        exit(0);
+    }
 }
 
 void sorteia_palavra(){
