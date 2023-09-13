f8561de58223061d78c01bda1476925cf9f1f1e8
patrickpbarroso <patrick.plbarroso@gmail.com>
2023-09-13 06:08:57 -0300

    Visualizando a palavra secreta a partir dos chutes

--- JogoDaForca/forca.cpp
+++ JogoDaForca/forca.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
+#include <map>
 using namespace std;
 
 const string PALAVRA_SECRETA ="MELANCIA";
+map<char, bool> chutou;
 
 bool letra_existe(char chute){
      for(char letra : PALAVRA_SECRETA){
@@ -13,14 +16,25 @@ bool letra_existe(char chute){
 }
 
 int main(){
-    cout << PALAVRA_SECRETA << endl;
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
     while(nao_acertou && nao_enforcou){
+        for(char letra: PALAVRA_SECRETA){
+            if(chutou[letra]){
+                cout << letra << " ";
+            }
+            else{
+                cout << "_ ";
+            }
+        }
+        cout << endl;
+
         char chute;
         cin >> chute;
+
+        chutou[chute] = true;
 
         if(letra_existe(chute)){
             cout << "Você acertou! Seu chute está na palavra." << endl;
